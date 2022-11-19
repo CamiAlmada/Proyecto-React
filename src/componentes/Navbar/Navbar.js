5dfb2b56b778098c3cccccb8988ecc8aa634a938
Camila Almada <camialmada23@gmail.com>
2022-11-19 19:29:34 -0300

    clase JSX

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+
+const Navbar = ()=>{
+    return (
+        <nav>
+          <h1 className="title">Mi Tienda Virtual</h1>
+          <button>Inicio</button>
+          <button>Contactos</button>
+          <button>Artículos</button>
+        </nav>
+    )
+
+}
+
+export default Navbar
